ec4098b8ac6c5140b371fd8b63e2901ba6f7732b
JeanJNAP <jeanjnap@gmail.com>
2018-04-03 12:04:56 -0300

    Implements login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  userDate:any = null;
+
+  constructor(
+    public navCtrl: NavController,
+    private facebook: Facebook
+  ) {
 
   }
+
+  Login() {
+    this.facebook.login(['email', 'public_profile']).then((response) => {
+      this.facebook.api('me?fields=picture.type(large),name,email',[]).then((profile) => { 
+        this.userDate = {
+          name: profile['name'],
+          email: profile['email'],
+          id: profile['id'],
+          picture: profile['picture']['data']['url']
+        };
+        //alert(`Nome: ${profile['name']}\nEmail: ${profile['email']}\nId: ${profile['id']}\nFoto: ${profile['picture']['data']['url']}`);
+      })
+    })
+  }
+
 
 }
